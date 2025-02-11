2fb4b51bf90191f9dade5bc667d94d4bb805ef6e
CauZy-Goes <cauafariasdev@gmail.com>
2025-02-11 14:42:20 -0300

    feat: add python review

---
+++ pythonando/app.py
@@ -0,0 +1,14 @@
+stringUm = "Hellou"
+stringDois = "World"
+
+print(type(stringUm))
+print(f"Olá = {stringUm}, Mundo = {stringDois}")
+
+stringtres = '3'
+intUm  = 12
+
+print(intUm + int(stringtres))
+
+print(6.6 // 2)
+
+print(11 ** 2)
